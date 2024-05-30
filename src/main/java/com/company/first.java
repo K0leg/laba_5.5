55d8076c69b46e5186ddefe325297ea78fafb811
K0leg <olegkovtun2005ua@gmail.com>
2024-05-30 13:02:14 +0300

    Add files via upload

--- src/main/java/com/company/first.java
+++ src/main/java/com/company/first.java
@@ -1,17 +1,25 @@
 package com.company;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class first {
+
+    public static void main(String[] args) {
+
+        String array[] = new String[5];
+
+        for (int i = 0; i < 5; i++) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Введіть речення: ");
+            String word = scanner.next();
+            array[i] = word;
         }
+
+        System.out.println(array[0].concat(array[1]).concat(array[2]).concat(array[3]).concat(array[4]));
+
+
+
+
     }
+
 }
